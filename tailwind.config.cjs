654cd49167066e95fca0b9f7ee17aeb33742627e
Kevin Lewis <kvn@lws.io>
2022-03-19 16:50:45 +0000

    Framework

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-    content: [
-        './public/**/*.html',
-        './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
-    ],
-    plugins: [
-        require('@tailwindcss/typography')
-    ]
+  mode: 'jit',
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        durable: '#009473',
+        flexible: '#0066FF',
+        practical: '#FF0066',
+        complex: '#FCAF3C',
+        compatible: '#FF4E00'
+      },
+      fontFamily: {
+        sans: ['Fredoka', ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
 };
